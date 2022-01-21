89cb28df32b786ca239327c1ba611807c3ea84ba
Cintakins <jacintalerwill@hotmail.com>
2022-01-21 14:47:15 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,28 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert('You clicked submit!');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
-})
+    runGame('addition');
+});
 
 /**
  * The main game loop when the page is loaded.
  * after the user's answer has been processed.
  */
-function runGame() {
+function runGame(gameType) {
     //create two random numbers
-    let num1 = math.floor(math.random() * 25) + 1;
-    let num2 = math.floor(math.random() * 25) + 1;
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -39,8 +47,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
