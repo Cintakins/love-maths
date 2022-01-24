b528e90566a461354c0a522a09a1433be84b2fda
Cintakins <jacintalerwill@hotmail.com>
2022-01-24 14:35:55 +0000

    added subract code

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,10 +25,12 @@ function runGame(gameType) {
 
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
-    } alse if (gameType === 'multiply') {
+    } else if (gameType === 'multiply') {
         displayMultiplyQuestion(num1, num2);
     }
-        else {
+        else if (gameType === 'subtract') {
+            displaySubtractQuestion(num1, num2);
+        }else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
     }
@@ -69,7 +71,9 @@ function calculateCorrectAnswer() {
     } else if (operator === 'x') {
         return [operand1 * operand2, 'multiply'];
     }
-        else {
+        else if ( operator === '-') {
+            return [operand1 - operand2, 'subtract'];
+        }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -101,7 +105,9 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion() {
-
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '-';
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
